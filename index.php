acdeb31cf87ebfc64b10715a13dcea22ce77e1eb
Sky <skyvorobey@icloud.com>
2020-11-12 17:15:27 +0200

    Оснваная страница, адаптированна и оптимизированно адаптированна и оптимизировано под safari, firefox, googlechrome

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style/style.css">
+    <title>skyvorobey.ru</title>
+</head>
+<body>
+    <main class="main">
+        <div class="main_wrap">
+            <div class="main_wrap-title">
+                <h1>Внимание!</h1>
+                <h2>Сайт на ремонте</h2>
+            </div>
+            <div class="main_wrap-container-img">
+                <img src="img/index/work_zone.png" width="511px" height="512px" class="main_wrap-img">
+
+                <form method="post" class="main_wrap-form">
+                    <input type="text" name="login" placeholder="Логин" autocomplete="off" class="main_wrap-form-item">
+                    <input type="submit" name="enter" placeholder="Вход" value="ВХОД" class="main_wrap-form-item main_wrap-form-item-btm">
+                    <input type="password" name="password" placeholder="Пароль" autocomplete="off" class="main_wrap-form-item">
+                </form>
+            </div>
+            <div class="main_wrap-telegram">
+                <div class="main_wrap-telegram-item">
+                    <a href="tg://resolve?domain=@skyvorobey">
+                        <img src="img/index/telegram.svg" width="50" height="50" class="main_telegram-img">
+                    </a>
+                </div>
+                <h3><a href="tg://resolve?domain=@skyvorobey">Связь со мной</a></h3>
+            </div>
+
+        </div>
+
+    </main>
+</body>
+</html>
